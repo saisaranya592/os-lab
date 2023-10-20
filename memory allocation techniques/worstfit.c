71743659ca6202d06e6c9a6c5ac59c9ddc8ffa1a
saisaranya592 <141509998+saisaranya592@users.noreply.github.com>
2023-10-20 23:40:30 +0530

    worstfit.c

--- memory allocation techniques/worstfit.c
+++ memory allocation techniques/worstfit.c
@@ -1,12 +1,9 @@
-#include<stdio.h> 
-#include<conio.h> 
+#include<stdio.h>  
 #define max 25 
-void main()
+int main()
 {
-int 
-frag[max],b[max],f[max],i,j,nb,nf,temp,highes 
-t=0; static int bf[max],ff[max];
-clrscr();
+int frag[max],b[max],f[max],i,j,nb,nf,temp,highest=0; 
+static int bf[max],ff[max];
 printf("\n\tMemory Management Scheme - Worst Fit"); 
 printf("\nEnter the number of blocks:");
 scanf("%d",&nb);
@@ -43,5 +40,4 @@ ff[i]=j; highest=temp;
 printf("\nFile_no:\tFile_size:\tBlock_no:\tBlock_size:\tFragement"); 
 for(i=1;i<=nf;i++)
 printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d",i,f[i],ff[i],b[ff[i]],frag[i]); 
-getch();
 }
